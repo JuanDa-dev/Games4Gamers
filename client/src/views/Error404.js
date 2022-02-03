4f663bc2d1c3c4cc54a837745003e0c9a16b867d
Edimar-star <edimarod@outlook.es>
2022-02-02 22:59:26 -0500

    added view page not found

--- client/src/views/Error404.js
+++ client/src/views/Error404.js
@@ -1,7 +1,17 @@
+import fondo from '../images/404.svg'
+import '../CSS/Error404.css'
 
 const Error404 = () => {
     return (
-        <div>Page not found</div>
+        <div className="row justify-content-around align-items-center contentError">
+            <img className="col-md-5 col-12 col-sm-5" src={fondo} />
+            <div className="col-md-4 col-12 col-sm-7 tab-cont">
+                <h2>Page not found</h2>
+                <div className="d-grid gap-2">
+                    <a href='/' className="btn btn-primary" type="submit" value="submit">Go to Home Page</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
